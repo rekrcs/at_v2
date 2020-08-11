9beede2ff9bacd407a4bdb2187412853fa72cf2c
rekrcs <rekrcs@gmail.com>
2020-08-11 20:31:11 +0900

    컨트롤러 테스트

--- src/main/java/com/sbs/bs/at_v2/controller/HomeController.java
+++ src/main/java/com/sbs/bs/at_v2/controller/HomeController.java
@@ -1,8 +1,19 @@
 package com.sbs.bs.at_v2.controller;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Controller
 public class HomeController {
@@ -13,16 +24,40 @@ public class HomeController {
 		return "안녕";
 	}
 
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/article")
 	@ResponseBody
-	public String showMain2() {
-		return "안녕2";
+	public Article showArticle() {
+
+		return new Article(1, "2020-07-28 12:12:12", "제목", "내용");
 	}
 
-	@RequestMapping("/usr/home/plus")
+	@RequestMapping("/usr/home/articleList")
 	@ResponseBody
-	public int showPlus(int a, int b) {
-		return a + b;
+	public List<Article> showArticleList() {
+		List<Article> list = new ArrayList<>();
+		list.add(new Article(1, "2020-07-28 12:12:12", "제목", "내용"));
+		list.add(new Article(2, "2020-07-28 12:12:12", "제목2", "내용2"));
+
+		return list;
 	}
 
+	@RequestMapping("/usr/home/map")
+	@ResponseBody
+	public Map<String, Object> showMap() {
+		Map<String, Object> map = new LinkedHashMap<>();
+		map.put("철수나이", 12);
+		map.put("영희나이", 22);
+		map.put("영숙나이", 32);
+		return map;
+	}
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+class Article {
+	private int id;
+	private String regDate;
+	private String title;
+	private String body;
 }
