779656cccf754c55b8c1320436f9298902c7f409
rekrcs <rekrcs@gmail.com>
2020-08-11 12:35:28 +0900

    생성

---
+++ src/main/java/com/sbs/bs/at_v2/AtV2Application.java
@@ -0,0 +1,13 @@
+package com.sbs.bs.at_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/bs/at_v2/AtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.bs.at_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
